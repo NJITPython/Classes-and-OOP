75942c5e47220548ec04857326c975c5dea6dae7
NJITPython <NJITPython@users.noreply.github.com>
2017-01-31 16:56:49 -0500

    Update Classes and OOP.py

--- Classes and OOP.py
+++ Classes and OOP.py
@@ -8,16 +8,26 @@
 # the sum of two elements defines the next set
 class Fibonacci():
     def __init__(self, a, b):
-        print('constructor')
+        print('constructor Init')
         self.a = a     # self.a - attach to an object (not class)
         self.b = b
+        print('Out of constructor')
 
     def series(self):
         while(True):
+            print('\n--',self.b,'\n')
             yield(self.b)
             self.a, self.b = self.b, self.a + self.b
+            print('\n@@@@',self.a," - ", self.b,'\n')
 
-f = Fibonacci(0, 1)
+
+
+
+
+print('Before Constructor')
+f = Fibonacci(0,1)
+print('After Constructor')
+
 for r in f.series():
     if r > 100: break    
-    print(r, end=' ')
+    print('****',r, end=' ')
